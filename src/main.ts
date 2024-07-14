7184a53d836571e60c190326181db32c1451963d
rango198 <137562811+rango198@users.noreply.github.com>
2024-07-14 22:03:03 +0300

    port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(coockieParser());
   app.enableCors({
-    origin: ['http://localhost:3002'],
+    origin: ['http://localhost:3000'],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
